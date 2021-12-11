4d6deca375c505c0e37d673c0456b428f702a4ce
WilSonS9 <54662723+WilSonS9@users.noreply.github.com>
2021-12-11 06:48:23 +0100

    Day 11

---
+++ aoc11part2.py
@@ -0,0 +1,76 @@
+f = open('./inp.txt').read().split('\n')
+
+coords = {}
+
+for i in range(len(f)):
+    for j in range(len(f)):
+        coords[(i, j)] = int(f[i][j])
+
+
+def step(coords, flashes):
+    newFlashes = flashes
+
+    def increaseNeighbours(coords, point, flashed):
+        newCoords = coords
+        newFlashed = flashed
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if not (i, j) == (0, 0):
+                    pointCoords = (point[0] + i, point[1] + j)
+                    if 0 <= pointCoords[0] <= 9 and 0 <= pointCoords[1] <= 9:
+                        newCoords[pointCoords] += 1
+        return newCoords, newFlashed
+
+    flashed = {k: False for k, _ in coords.items()}
+    coords = {k: 1+v for k, v in coords.items()}
+
+    def printSpace(coords):  # For debugging
+        for i in range(10):
+            row = ''
+            for j in range(10):
+                row += str(coords[(i, j)])
+                row += ' '
+            print(row)
+
+    def checkFlash(coords, flashed, flashes):
+        newCoords, newFlashed, newFlashes = coords, flashed, flashes
+        for i in range(10):
+            for j in range(10):
+                point = (i, j)
+                if coords[point] > 9 and not flashed[point]:
+                    newCoords, newFlashed = increaseNeighbours(
+                        coords, point, flashed)
+                    newFlashes += 1
+                    newFlashed[point] = True
+                    return newCoords, newFlashed, True, newFlashes
+        return newCoords, newFlashed, False, newFlashes
+
+    cont = True
+    while cont:
+        coords, flashed, cont, newFlashes = checkFlash(
+            coords, flashed, newFlashes)
+
+    sim = False
+
+    nZero = 0
+    for k, v in flashed.items():
+        if v:
+            coords[k] = 0
+            nZero += 1
+
+    if nZero == 100:
+        sim = True
+
+    return coords, newFlashes, sim
+
+
+flashes = 0
+k = 0
+sim = False
+
+while True:
+    k += 1
+    coords, flashes, sim = step(coords, flashes)
+    if sim:
+        print(k)
+        break
